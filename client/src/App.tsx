f85341bcdb0a1dc239803c4de880487c5b239e80
Ruman <rumanbsl@gmail.com>
2021-08-28 19:18:49 +0300

    Remove unnecessary type assertions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-import * as grpcWeb from "grpc-web";
 import { GreeterClient } from "./Interfaces/generated/helloworld_grpc_web_pb";
-import { Request, Reply } from "./Interfaces/generated/helloworld_pb";
+import { Request } from "./Interfaces/generated/helloworld_pb";
 
 const echoService = new GreeterClient("http://127.0.0.1:9999", null, null);
 
 const request = new Request();
 request.setTitle("Ruman");
 
-const call = echoService.sayHello(request, { "custom-header-1": "value1" }, (err: grpcWeb.Error, response: Reply) => {
+const call = echoService.sayHello(request, { "custom-header-1": "value1" }, (_, response) => {
   console.log(response.getMsg());
 });
-call.on("status", (status: grpcWeb.Status) => {
+call.on("status", (status) => {
   console.log(status);
 });
 
